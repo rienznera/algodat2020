ab25c8ed0cc21aad92976137b22b8e8b66919e16
Alex <alrienzner@edu.aau.at>
2020-04-21 11:45:22 +0200

    EX1 refactured

--- Abgabe01/code/src/main/java/ab1/impl/Angermann_RIenzner/Ab1Impl.java
+++ Abgabe01/code/src/main/java/ab1/impl/Angermann_RIenzner/Ab1Impl.java
@@ -9,13 +9,11 @@ public class Ab1Impl implements Ab1 {
 	@Override
 	public int binarySearch(int[] data, int element) {
 
-		//Arrays.binarySearch(data, element);
-
 		int low = 0;
 		int high = data.length - 1;
 
 		while (low <= high) {
-			int mid = (low + high) >>> 1;
+			int mid = (low + high) /2;
 			int midVal = data[mid];
 
 			if (midVal < element)
@@ -23,9 +21,9 @@ public class Ab1Impl implements Ab1 {
 			else if (midVal > element)
 				high = mid - 1;
 			else
-				return mid; // key found
+				return mid; // Eintrag gefunden
 		}
-		return -1;  // key not found.
+		return -1;  // Eintrag nicht gefunden
 
 
 	}
