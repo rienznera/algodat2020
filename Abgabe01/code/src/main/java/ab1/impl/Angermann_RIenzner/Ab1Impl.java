d7fda4a3c39417f6b4b9669e8df427e1d03f3587
Alex <alrienzner@edu.aau.at>
2020-04-21 11:43:19 +0200

    EX1
    Punkte: 2

--- Abgabe01/code/src/main/java/ab1/impl/Angermann_RIenzner/Ab1Impl.java
+++ Abgabe01/code/src/main/java/ab1/impl/Angermann_RIenzner/Ab1Impl.java
@@ -2,12 +2,35 @@ package ab1.impl.Angermann_RIenzner;
 
 import ab1.Ab1;
 
+import java.util.Arrays;
+
 public class Ab1Impl implements Ab1 {
 
 	@Override
 	public int binarySearch(int[] data, int element) {
-		return 0;
+
+		//Arrays.binarySearch(data, element);
+
+		int low = 0;
+		int high = data.length - 1;
+
+		while (low <= high) {
+			int mid = (low + high) >>> 1;
+			int midVal = data[mid];
+
+			if (midVal < element)
+				low = mid + 1;
+			else if (midVal > element)
+				high = mid - 1;
+			else
+				return mid; // key found
+		}
+		return -1;  // key not found.
+
+
 	}
+
+
 
 	@Override
 	public void shellSort(Integer[] data) {
--- Abgabe01/code/src/test/java/ab1/Ab1Test.java
+++ Abgabe01/code/src/test/java/ab1/Ab1Test.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Ab1Test {
